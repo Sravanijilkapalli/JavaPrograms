5c9dd4d39fc2e1537f20701370e740c1b3c54520
sravanijilkapalli <sravanireddyjilkapalli@gmai.com>
2022-02-09 15:03:49 -0800

    added the changes in helloworld

--- eclipse-workspace/HelloWorld/src/forLoop/For.java
+++ eclipse-workspace/HelloWorld/src/forLoop/For.java
@@ -18,10 +18,11 @@ public class For {
 		
 		even(2 , 20);
 		
+		twoStringSum("123456" , "23456");
+			String jsr = twoString("123456789024" , "31");
 		
-			
-		
-		
+			System.out.println(jsr);
+
 	}
 
 
@@ -81,6 +82,47 @@ public class For {
 		System.out.println(sum);
 
 		
+	}
+	
+	public static void twoStringSum(String s , String r) {
+		
+		
+		int sum = Integer.parseInt(s) + Integer.parseInt(r);
+		
+		
+		System.out.println(sum);
+	}
+	
+	public static  String twoString(String num1 , String num2) {
+		
+		StringBuilder result = new StringBuilder();
+		int i= num1.length()-1;
+		int j = num2.length()-1;
+		 int carry=0;
+
+		 while(i>=0 || j >=0 ) {
+			 
+			 int sum = carry;
+			 if(i>=0) {
+				 
+				 sum += num1.charAt(i--) - '0';
+				 
+			 }
+			if(j>=0) {
+				
+				 sum += num2.charAt(j--) - '0';
+				
+			}
+			 result.append(sum%10);
+			 carry = sum/10;
+		 }
+		
+		if(carry!=0) {
+			
+			result.append(carry);
+		}
+		
+		return result.reverse().toString();
 	}
 	
 }
--- eclipse-workspace/HelloWorld/src/hello/HelloWorld.java
+++ eclipse-workspace/HelloWorld/src/hello/HelloWorld.java
@@ -48,7 +48,12 @@ public class HelloWorld {
 		
 		//System.out.println(rr);
 		
-		
+		String jsr = twoStringsum("123456789" , "234567890");
+		String jsr1 = twoBinarySum("1011101" , "1010111");
+
+		System.out.println(jsr);
+		System.out.println(jsr1);
+
 	}
 	
 	
@@ -86,5 +91,80 @@ public class HelloWorld {
 		
 	}
 	
+	public static String twoStringsum(String num1 , String num2) {
+		
+		StringBuilder result = new StringBuilder();
+		
+		int carry=0;
+		
+		int i= num1.length()-1;
+		int j= num2.length()-1;
+		
+		while(i>=0|| j>=0) {
+			
+			int sum=carry;
+			
+			
+			if(i>=0) {
+				
+				sum += num1.charAt(i--) - '0';
+				
+			}
+			if(j>=0) {
+				
+				sum += num2.charAt(j--) - '0';
+
+			}
+			
+			result.append(sum%10);
+			
+			carry = sum/10;
+		}if(carry!=0) {
+			
+			result.append(carry);
+		}
+		
+		return result.reverse().toString();
+
+	}
 	
+	public static String twoBinarySum(String num1 , String num2) {
+		
+		StringBuilder result = new StringBuilder();
+		
+		int i = num1.length()-1;
+		int j= num2.length()-1;
+		
+		int carry =0;
+		
+		while(i>=0 || j>=0) {
+			
+			int sum = carry;
+			
+			if(i>=0) {
+				
+				sum += num1.charAt(i--) - '0';
+				
+			}
+			if(j>=0) {
+				
+				sum += num2.charAt(j--) - '0';
+				
+			}
+			
+			result.append(sum%2);
+			
+			carry = sum/2;
+			
+		}
+		if(carry!=0) {
+			
+			result.append(carry);
+
+		}
+		
+		return result.reverse().toString();
+
+		
+	}
 }
